b7e2c2365308c16fb0a8e162b90926c01e4d022c
Sean Cline <smcline06@gmail.com>
2021-06-11 18:36:43 -0700

    nope

--- build.sh
+++ build.sh
@@ -17,31 +17,31 @@ echo "TAG: $tag"
 # Build server
 version=`cat $PWD/llama-server/version`
 docker build $PWD/llama-server -t smcline06/llama-server:${tag}${version}
-docker build $PWD/llama-server -t smcline06/llama-server:latest
+docker build $PWD/llama-server -t smcline06/llama-server:${tag}latest
 
 docker push smcline06/llama-server:${tag}${version}
-docker push smcline06/llama-server:latest
+docker push smcline06/llama-server:${tag}latest
 
 # Build client
 version=`cat $PWD/llama-client/version`
 docker build $PWD/llama-client -t smcline06/llama-client:${tag}${version}
-docker build $PWD/llama-client -t smcline06/llama-client:latest
+docker build $PWD/llama-client -t smcline06/llama-client:${tag}latest
 
 docker push smcline06/llama-client:${tag}${version}
-docker push smcline06/llama-client:latest
+docker push smcline06/llama-client:${tag}latest
 
 # Build scraper
 version=`cat $PWD/llama-scraper/version`
 docker build $PWD/llama-scraper -t smcline06/llama-scraper:${tag}${version}
-docker build $PWD/llama-scraper -t smcline06/llama-scraper:latest
+docker build $PWD/llama-scraper -t smcline06/llama-scraper:${tag}latest
 
 docker push smcline06/llama-scraper:${tag}${version}
-docker push smcline06/llama-scraper:latest
+docker push smcline06/llama-scraper:${tag}latest
 
 # Build probe
 version=`cat $PWD/llama-probe/version`
 docker build $PWD/llama-probe -t smcline06/llama-probe:${tag}${version}
-docker build $PWD/llama-probe -t smcline06/llama-probe:latest
+docker build $PWD/llama-probe -t smcline06/llama-probe:${tag}latest
 
 docker push smcline06/llama-probe:${tag}${version}
-docker push smcline06/llama-probe:latest
+docker push smcline06/llama-probe:${tag}latest
