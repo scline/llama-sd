3e75c03c62f6e5f480b7d7570490570d38c071f1
Sean Cline <smcline06@gmail.com>
2021-06-17 11:49:41 -0700

    Fixing scraper not updating after initial startup

--- llama-scraper/entrypoint.sh
+++ llama-scraper/entrypoint.sh
@@ -6,15 +6,13 @@ server_url="$LLAMA_SERVER/api/v1/scraper"
 echo "SERVER: $LLAMA_SERVER"
 echo "Scraper URL: $server_url"
 
-# Empty variable to start
-collector_hosts=""
-
 # Grab a new configuration from the server
 echo "Waiting 60 seconds before pulling a list of hosts to scrape..."
 sleep 60
 
-# Host list
+# Initial host list setup
 collector_hosts_new=`curl -s $server_url`
+collector_hosts=$collector_hosts_new
 
 # Output for debugging
 echo "InfluxDB Host: $INFLUXDB_HOST"
@@ -23,16 +21,13 @@ echo "InfluxDB Name: $INFLUXDB_NAME"
 echo "Host List: $collector_hosts_new"
 
 echo "Starting Scraper"
-scraper -llama.collector-hosts $collector_hosts_new -llama.collector-port 8100 -llama.influxdb-host $INFLUXDB_HOST -llama.influxdb-name $INFLUXDB_NAME -llama.influxdb-port $INFLUXDB_PORT  -llama.interval 10
+scraper -llama.collector-hosts $collector_hosts_new -llama.collector-port 8100 -llama.influxdb-host $INFLUXDB_HOST -llama.influxdb-name $INFLUXDB_NAME -llama.influxdb-port $INFLUXDB_PORT -llama.interval 10
 
 echo "~~~ Config Checking Loop ~~~"
 while true
 do
   # Sleep for 60 seconds, do this first so we dont have issues at startup
   sleep 60
-
-  # Assign old values to variable for later comparing
-  collector_hosts=$collector_hosts_new
 
   # New host list
   collector_hosts_new=`curl -s $server_url`
@@ -47,11 +42,15 @@ do
   # If (running ISNOTEQUALTO temp)
   if [[ "$collector_hosts" != "$collector_hosts_new" ]]; then
     echo "Config update found!"
-    echo "Host List: $collector_hosts_new"
+    echo "Old Host List: $collector_hosts"
+    echo "New Host List: $collector_hosts_new"
 
     # Kill running scaper for updated targets
     kill -9 `ps -A -o pid,cmd | grep collector | grep -v grep | head -n 1 | awk '{print $1}'`
     scraper -llama.collector-hosts $collector_hosts_new -llama.collector-port 8100 -llama.influxdb-host $INFLUXDB_HOST -llama.influxdb-name $INFLUXDB_NAME -llama.influxdb-port $INFLUXDB_PORT  -llama.interval 10 &
+ 
+   # Update new list
+    collector_hosts=$collector_hosts_new
   fi 
 
 done
