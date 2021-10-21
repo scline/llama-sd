1f01e82bb0259a7579e82c2119b567e60254087d
Stephen Ellis <stephen.d.ellis@gmail.com>
2021-10-20 17:38:01 -0700

    Starting on registration

--- llama-probe/lamoid/alpaca/graze.go
+++ llama-probe/lamoid/alpaca/graze.go
@@ -1,10 +1,14 @@
 package alpaca
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func (g *LamoidEnv) ValidateEnvironment() {
@@ -24,6 +28,47 @@ func (g *LamoidEnv) StartReflector() {
 	}
 
 	g.ReflectorPID = reflector.Process.Pid
+}
+
+func (g *LamoidEnv) GrazeAnatomy() {
+
+	lamoidAnatomy := &PayLoad{
+		Port:      g.Port,
+		Keepalive: g.KeepAlive,
+		Ip:        g.SourceIP,
+		Group:     g.Group,
+	}
+
+	lamoidAnatomy.Tags.Version = "0.1.0"
+	lamoidAnatomy.Tags.ProbeName = g.ProbeName
+	lamoidAnatomy.Tags.ProbeShortname = g.ProbeShortName
+
+	// Convert PayLoad struct to JSON now that we have all our values.
+	byteArray, err := json.Marshal(lamoidAnatomy)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// POST JSON to API Server
+	url := envs["LLAMA_SERVER"] + "/api/v1/register"
+	log.Println("Server URL:", url)
+
+	// Print registration JSON we send to API server
+	log.Println("JSON Post:", string(byteArray))
+
+	// Loop the registration every 60 seconds
+	request, error := http.NewRequest("POST", url, bytes.NewBuffer(byteArray))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{Timeout: 5 * time.Second}
+	response, error := client.Do(request)
+	if error != nil {
+		log.Println(error)
+	}
+	defer response.Body.Close()
+
+	// Log responce
+	log.Println("Response Status:", response.Status)
 }
 
 func (g *LamoidEnv) StartCollector() {
--- llama-probe/lamoid/alpaca/structs.go
+++ llama-probe/lamoid/alpaca/structs.go
@@ -16,10 +16,11 @@ type PayLoad struct {
 // LamoidEnv struct containing the running environment information
 // for the grazzing llama probe.
 type LamoidEnv struct {
+	SourceIP       string `env:LLAMA_SOURCE_IP`
 	Server         string `env:LLAMA_SERVER`
 	Group          string `env:LLAMA_GROUP`
-	Port           uint32 `env:LLAMA_PORT`
-	KeepAlive      uint32 `env:LLAMA_KEEPALIVE`
+	Port           int    `env:LLAMA_PORT`
+	KeepAlive      int    `env:LLAMA_KEEPALIVE`
 	ProbeName      string `env:PROBE_NAME`
 	ProbeShortName string `env:PROBE_SHORTNAME`
 	ServerURL      string
