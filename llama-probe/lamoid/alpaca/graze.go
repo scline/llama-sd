98c48ae1a26b176aa3acaa13e0d3b312a9a1c9bd
Stephen Ellis <stephen.d.ellis@gmail.com>
2021-10-21 18:12:26 -0700

    documentation is key.....

--- llama-probe/lamoid/alpaca/graze.go
+++ llama-probe/lamoid/alpaca/graze.go
@@ -16,23 +16,11 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func (g *LamoidEnv) StartReflector() {
-	// Start llama reflector and update the process id ref.
-	reflector := exec.Command("reflector", fmt.Sprintf("-port %v", g.Port))
-
-	reflector.Stdout = os.Stdout
-
-	err := reflector.Start()
-
-	if err != nil {
-		log.Fatalf("[LLAMA-REFLECTOR]: There was an error starting the reflector, %s", err)
-	}
-
-	g.ReflectorPID = reflector.Process.Pid
-}
-
+//GrazeAnatomy - A method called on LamoidEnv which registers the current running LLAMA configuration
+//to the LLAMA-SERVER
 func (g *LamoidEnv) GrazeAnatomy() {
 
+	//Build the registration Payload
 	lamoidAnatomy := &PayLoad{
 		Port:      g.Port,
 		Keepalive: g.KeepAlive,
@@ -40,8 +28,7 @@ func (g *LamoidEnv) GrazeAnatomy() {
 		Group:     g.Group,
 	}
 
-	//TODO: Read in version number at compile, add to LamoidEnv struct
-	//so its available to all methods.
+	//TODO: Read in version number
 	lamoidAnatomy.Tags.Version = "0.1.0"
 	lamoidAnatomy.Tags.ProbeName = g.ProbeName
 	lamoidAnatomy.Tags.ProbeShortname = g.ProbeShortName
@@ -51,76 +38,113 @@ func (g *LamoidEnv) GrazeAnatomy() {
 		log.Println(err)
 	}
 
-	//Might as well validate that the string is a URL since its comming from the
-	//Deployment Environment
+	//Build and Validate the LLAMA-SERVER url
 	serverURL, err := url.ParseRequestURI(fmt.Sprintf("%s/api/v1/register", g.ServerURL))
 	if err != nil {
-		log.Fatalf("[GRAZE-URL]: The url constructed was not a valid URI, check LLAMA_SERVER, %s", err)
+		log.Fatalf("[LAMOID-REGISTER]: The url constructed was not a valid URI, check LLAMA_SERVER, %s", err)
 	}
 
+	//Build the HTTP Post request
 	request, err := http.NewRequest("POST", serverURL.String(), bytes.NewBuffer(byteArray))
 
 	if err != nil {
-		log.Printf("[GRAZE-CLIENT]: There was a problem creating a new request object, %s", err)
+		log.Printf("[LAMOID-REGISTER]: There was a problem creating a new request object, %s", err)
 	}
 
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
+	//HTTP Client
 	client := &http.Client{
 		Timeout: 5 * time.Second,
 	}
 
+	//Process HTTP request and log the status
 	response, err := client.Do(request)
 
 	if err != nil {
-		log.Printf("[GRAZE-CLIENT]: There was a problem making a request, %s", err)
+		log.Printf("[LAMOID-REGISTER]: There was a problem making a request, %s", err)
 	}
 
 	defer func() {
 		err := response.Body.Close()
 
 		if err != nil {
-			log.Printf("[GRAZE-CLIENT]: There was a problem closing the response from LLAMA Server, %s", err)
+			log.Printf("[LAMOID-REGISTER]: There was a problem closing the response from LLAMA Server, %s", err)
 		}
 	}()
 
-	log.Printf("Response Status: %s", response.Status)
+	log.Printf("[LAMOID-REGISTER]Response Status: %s", response.Status)
 }
 
+//StartReflector - A method called on LamoidEnv which starts the LLAMA Reflector application and updates LamoidEnv with
+//its OS process identification (PID)
+func (g *LamoidEnv) StartReflector() {
+
+	// Build os exec command to launch reflector with a given param
+	reflector := exec.Command("reflector", fmt.Sprintf("-port %v", g.Port))
+
+	// Set the process to output to Standard Out
+	reflector.Stdout = os.Stdout
+
+	// Execute the exec command to start reflector, panic on error.
+	err := reflector.Start()
+
+	if err != nil {
+		log.Fatalf("[LLAMA-REFLECTOR]: There was an error starting the reflector, %s", err)
+	}
+
+	//Set PID
+	g.ReflectorPID = reflector.Process.Pid
+}
+
+//StartCollector - A method called on LamoidEnv which starts the LLAMA Collector application and updates LamoidEnv with
+//its OS process identification (PID)
 func (g *LamoidEnv) StartCollector() {
-	// Start llama collector and update the process id ref.
+
+	// Build os exec command to launch colelctor with a given param
 	collector := exec.Command("collector", "-llama.config /opt/alpaca/config.yaml")
 
+	// Set the process to output to Standard Out
 	collector.Stdout = os.Stdout
 
+	// Execute the exec command to start colelctor, panic on error.
 	err := collector.Start()
 
 	if err != nil {
 		log.Fatalf("[LLAMA-COLLECTOR]: There was an error starting the collector, %s", err)
 	}
 
+	//Set PID
 	g.CollectorPID = collector.Process.Pid
 }
 
+//GrazeConfig - A method called on LamoidEnv which retrieves the running configuration from the LLAMA Servers configuration
+//API. Writes that config as a YAML to the local node for consumption by the collector process. Must be ran before the
+//collector is started.
 func (g *LamoidEnv) GrazeConfig() {
-	// Fetch Config write to yaml on local host
+
+	// Build and validate URL
 	configReqURL, err := url.ParseRequestURI(fmt.Sprintf("%s/api/v1/config/%s", g.ServerURL, g.Group))
 	if err != nil {
 		log.Fatalf("[CONFIG-URL]: The url constructed was not a valid URI, check LLAMA_SERVER & LLAMA_GROUP , %s", err)
 	}
 
+	// Configure request url params
 	configReqParam := url.Values{}
 	configReqParam.Add("llamaport", fmt.Sprintf("%v", g.Port))
 
+	// Build request
 	request, err := http.NewRequest("GET", configReqURL.String(), strings.NewReader(configReqParam.Encode()))
 	if err != nil {
 		log.Printf("[CONFIG-CLIENT]: There was a problem creating a new request object, %s", err)
 	}
 
+	//HTTP Client
 	client := &http.Client{
 		Timeout: time.Second * 5,
 	}
 
+	// Process HTTP request
 	response, err := client.Do(request)
 	if err != nil {
 		log.Printf("[CONFIG-CLIENT]: There was a problem making a request to LLAMA Server, %s", err)
@@ -134,13 +158,16 @@ func (g *LamoidEnv) GrazeConfig() {
 		}
 	}()
 
+	// Read response into bytes
 	respBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Printf("[CONFIG-CLIENT]: There was a problem reading the config response from LLAMA_SERVER, %s", err)
 	}
 
-	//Need to really play around with this to see if we can preserve the YAML formatting
+	//Note: Need to really play around with this to see if we can preserve the YAML formatting
 	//returned from LLAMA
+
+	//Serialize YAML Data into a struct
 	configRaw := LLamaConfig{}
 
 	yamlErr := yaml.Unmarshal(respBytes, &configRaw)
