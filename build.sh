d4c408e90d6ca9df0b6959129f286ecaa20eedb7
Stephen Ellis <stephen.d.ellis@gmail.com>
2021-10-28 23:17:22 -0700

    built not bought....

--- build.sh
+++ build.sh
@@ -19,16 +19,10 @@ version=`cat $PWD/llama-server/version`
 docker build $PWD/llama-server -t smcline06/llama-server:${tag}${version}
 docker build $PWD/llama-server -t smcline06/llama-server:${tag}latest
 
-docker push smcline06/llama-server:${tag}${version}
-docker push smcline06/llama-server:${tag}latest
-
 # Build scraper
 version=`cat $PWD/llama-scraper/version`
 docker build $PWD/llama-scraper -t smcline06/llama-scraper:${tag}${version}
 docker build $PWD/llama-scraper -t smcline06/llama-scraper:${tag}latest
-
-docker push smcline06/llama-scraper:${tag}${version}
-docker push smcline06/llama-scraper:${tag}latest
 
 # Build probe
 version=`cat $PWD/llama-probe/version`
